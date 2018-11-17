4751ecec105b8af91fa829caa6fcad9e8f8bd285
ukolovpk <ukolovpk@gmail.com>
2018-11-17 19:40:06 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,1 +1,7 @@
-dictionary = {"token": "12312awsedqwed"}
+dictionary = {
+    "token": "TOKEN",
+    "authorized users": ["UID1", "UID2"]
+}
+GET_TEMPERATURE = "/get_temperature"
+GET_HUMIDITY = "/get_humidity"
+GET_ALL = "/get_current_report"
