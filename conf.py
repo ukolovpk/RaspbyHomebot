af9278003eea21785349aebf534f93c19844c88b
ukolovpk <ukolovpk@gmail.com>
2018-11-18 20:20:57 +0000

    Update conf.py
    Добавлены команды включения/выключения "ночного" режима.

--- conf.py
+++ conf.py
@@ -5,3 +5,5 @@ dictionary = {
 GET_TEMPERATURE = "/get_temperature"
 GET_HUMIDITY = "/get_humidity"
 GET_ALL = "/get_current_report"
+ENABLE_NIGHT_MODE = "/enable_night_mode"
+DISABLE_NIGHT_MODE = "/disable_night_mode"
